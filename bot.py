f9f037b502c46b035fb4aae09f2285b6e6dfb526
Farris Ahmad <34035056+FarrisAhmad@users.noreply.github.com>
2021-02-27 18:19:42 -0500

    added version of bot.py with api token information

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import tweepy
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+auth = tweepy.OAuthHandler("9sWBI5513kq2H4w3Do6la9TgM", "4jdmsjKxyJWA6EuMy1Y5oBhMISq03qOzALk5xGUwbky5l9YyCu")
+auth.set_access_token("806902782531870722-R1eu6ZP72fwyfV3RXM7eqM0KhNyhrXb", "2Z9b1nPqzeslANsGBlSkggA8FdRpzczg9EcfoHXRZp7dk")
 
 # Create API object
 api = tweepy.API(auth)
 
 # Create a tweet
-api.update_status("Hello Tweepy")
+api.update_status("Hello world")
