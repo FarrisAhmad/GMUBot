ec5132d22f29e4dca29cec05a04eedcf9c5f72b2
Farris Ahmad <34035056+FarrisAhmad@users.noreply.github.com>
2021-04-30 20:39:56 -0400

    Add files via upload
    filter

--- bot.py
+++ bot.py
@@ -1,11 +1,34 @@
 import tweepy
-
+from better_profanity import profanity
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler("9sWBI5513kq2H4w3Do6la9TgM", "4jdmsjKxyJWA6EuMy1Y5oBhMISq03qOzALk5xGUwbky5l9YyCu")
-auth.set_access_token("806902782531870722-R1eu6ZP72fwyfV3RXM7eqM0KhNyhrXb", "2Z9b1nPqzeslANsGBlSkggA8FdRpzczg9EcfoHXRZp7dk")
+auth = tweepy.OAuthHandler("qVqsTAtCQqTrxBZ0YPjpc8JJz", "jIzg63IgE4GLeNkBiMkmLJQNXu2llFEd0gS7KSqycNFzJBGBON")
+auth.set_access_token("1359691434299916288-6YLZrkIIFDYCwZngZZZq6mT8Kje3oE", "AvPZ89UNBGYCkOXWPTumrPtA6n9YrXg4WRGiW3rdiISpK")
 
 # Create API object
 api = tweepy.API(auth)
 
-# Create a tweet
-api.update_status("Hello world")
+direct_messages = api.list_direct_messages()
+print(direct_messages)
+copy = direct_messages.copy()
+print(copy)
+for item in copy:
+    print(item)
+    api.update_status(str(item.message_create['message_data']['text']))
+    api.destroy_direct_message(item.id)
+mentions = api.mentions_timeline().copy()
+for item in mentions:
+    if (not (api.get_status(item.id).retweeted)) and (not profanity.contains_profanity(api.get_status(item.id).text)):
+        api.retweet(item.id)
+
+# api.update_status(direct_messages
+
+#
+# api qVqsTAtCQqTrxBZ0YPjpc8JJz
+# api secret jIzg63IgE4GLeNkBiMkmLJQNXu2llFEd0gS7KSqycNFzJBGBON
+#
+#
+# bearer AAAAAAAAAAAAAAAAAAAAAI6eNgEAAAAAQBDeNErgn1lIMw0ORDAYeMuPHyc%3DI0BY5rwIJ4DR9RnFyhy71XjQ4FmVpoGe909zGGL1Ch6XPr4BNn
+# access 1359691434299916288-6YLZrkIIFDYCwZngZZZq6mT8Kje3oE
+#
+# access secret AvPZ89UNBGYCkOXWPTumrPtA6n9YrXg4WRGiW3rdiISpK
+
